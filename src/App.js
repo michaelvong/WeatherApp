7c4d463b28b30412b9ef0ff8c1ab143601229526
michaelvong <mvong006@ucr.edu>
2023-05-02 15:25:53 -0700

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Weather App
+          Weather App test
         </p>
         <a
           className="App-link"
