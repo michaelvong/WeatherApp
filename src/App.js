75db20af0eaf7edb1403a674839d3d376d0c6d16
michaelvong <mvong006@ucr.edu>
2023-05-02 23:43:38 -0700

    finished weather details

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import React, {useEffect, useState} from "react";
-import Weather from './components/currentWeather';
 import axios from 'axios'
-
+import Forecast from './components/forecast'
 function App() {
 
   const [currentLat, setCurrLat] = useState([]);
   const [currentLong, setCurrLong] = useState([]);
   const [data, setData] = useState([]);
-  const [location, setLocation] = useState('')
+  const [location, setLocation] = useState('');
+  const [forecastData, setForecastData] = useState([]);
 
   //gets current long/lat and api calls to get weather at location
   useEffect(() => {
@@ -31,9 +31,17 @@ function App() {
     axios.get(`${process.env.REACT_APP_API_URL}q=${location}&units=imperial&APPID=${process.env.REACT_APP_API_KEY}`).then((response) =>
     {
       setData(response.data)
-      console.log(location)
-      console.log(response.data)
+      //console.log(location)
+      //console.log(response.data)
     })
+
+    axios.get(`${process.env.REACT_APP_API_URL2}q=${location}&units=imperial&APPID=${process.env.REACT_APP_API_KEY}`).then((response1) =>
+    {
+      setForecastData(response1.data)
+      //console.log(location)
+      console.log(response1.data)
+    })
+
   }
 
 
@@ -46,26 +54,39 @@ function App() {
         value={location}
         onChange={event => setLocation(event.target.value)}
         type="text"/>
-        <button className='Btn' onClick={getWeather}>Search</button>
+        <button className='Btn' onClick={getWeather}></button>
       </div>
-      
-      <div className='info'>
-        <div className='location'>
-          <p>{data.name}</p>
-        </div>
+
+      <div className='location'>
+          <h1>{data.name}</h1>
+      </div>
+
+      {data.main ? <div className='info'>
         <div className='temperature'>
-          {data.main ? <p> {data.main.temp}°F</p>: null}
+          <p> {data.main.temp}°F</p>
+          <p>Temperature</p>
         </div>
         <div className='feels'>
-          {data.main ? <p>Feels like: {data.main.feels_like}°F</p>: null}
+          <p>{data.main.feels_like}°F</p>
+          <p>Feels Like</p>
         </div>
         <div className='humid'>
-          {data.main ? <p>Humidity: {data.main.humidity}%</p>: null}
+          <p>{data.main.humidity}%</p>
+          <p>Humidity</p>
         </div>
         <div className='wind'>
-          {data.main ? <p>Winds: {data.wind.speed}mph</p>: null}
+          <p>{data.wind.speed}mph</p>
+          <p>Winds</p>
         </div>
-      </div>
+      </div> : null}
+
+
+      {forecastData.list ? <div className='forecast'> 
+        <h1>Forecast</h1>
+        <div className='futureData'>
+          <Forecast data = {forecastData}/>
+        </div>
+      </div> : null}
     </div>
   );
 }
---
+++ src/components/forecast.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from "react";
+
+const Forecast = (props) => {
+
+    const getForecast = () => {
+        
+    }
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Forecast;
